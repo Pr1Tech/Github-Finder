adc14b82b3b450b2cb1ed4e23904b5a6625b3758
selim <slmgn3r@gmail.com>
2023-01-08 00:12:34 +0300

    first commit

---
+++ src/components/NotFound.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+const NotFound =()=>{
+    return(
+        <div className='container pt-3'>
+            <h1>404 - NOT FOUND</h1>
+            <p className='lead'>Aradığınız sayfa bulunamadı.</p>
+        </div>
+    )
+}
+
+export default NotFound
